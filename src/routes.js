c57ba99c00b71de8f7610db600d861dc8c803b4d
GeorgiPetrov23 <georgip114@gmail.com>
2026-04-15 19:32:56 +0300

    Add register page

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,14 @@ import { Router } from 'express';
 
 import homeController from './controllers/homeController.js';
 import recipeController from './controllers/recipeController.js';
+import authController from './controllers/authController.js';
 
 const router = Router();
 
 router.use('/', homeController);
 router.use('/recipes', recipeController);
+router.use('/auth', authController);
+
 router.use((req, res) => {
     res.render('404', { title: '404 Not Found' });
 });
