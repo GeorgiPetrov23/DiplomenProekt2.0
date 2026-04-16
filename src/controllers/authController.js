67828df8e8efb9f10fc9f961f13015de76a5dfc7
GeorgiPetrov23 <georgip114@gmail.com>
2026-04-16 11:12:01 +0300

    Save registered user with hashed password in DB

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
+import authService from '../services/authService.js';
 
 const router = Router();
 
 router.get('/register', (req, res) =>{
     res.render('auth/register', { title: 'Register Page' });
-})
+});
+
+router.post('/register', async (req, res) => {
+    const { username, email, password, rePassword } = req.body;
+
+    await authService.register(username, email, password);
+    // console.log(username);
+    // console.log(email);
+    // console.log(password);
+    // console.log(rePassword);
+
+    res.redirect('/auth/login');
+});
+    
 
 export default router;
